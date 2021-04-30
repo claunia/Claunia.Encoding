e7f8f44043cf366c9e06f700816ecbf87266454c
Natalia Portillo <claunia@claunia.com>
2021-04-30 12:04:37 +0100

    Update PETSCII to include Unicode 13.0 symbols.

--- Claunia.Encoding/PETSCII.cs
+++ Claunia.Encoding/PETSCII.cs
@@ -24,13 +24,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System.Text;
+
 namespace Claunia.Encoding
 {
     /// <summary>
     ///     Represents an Commodore PET Standard Code for Information Interchange (aka CBM ASCII) character encoding of
     ///     Unicode characters.
     /// </summary>
-    public class Petscii : SingleByteEncoding
+    public class Petscii : SingleByteEncodingWithRunes
     {
         public override string BodyName        => "petscii";
         public override int    CodePage        => 0;
@@ -48,111 +50,143 @@ namespace Claunia.Encoding
 
         /// <summary>The PETSCII to Unicode character map, unshifted (default) variant.</summary>
         /// <remarks>Reference used: http://style64.org/petscii/ and others.</remarks>
-        protected override char[] CharTable => new[]
+        protected override Rune[] CharTable => new[]
         {
             // 0x00
-            '\u0000', '\u0000', '\u0000', '\u0003', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0003), new Rune(0x0000), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x08
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u000A', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x000A),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x10
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0008', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0008), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x18
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x20
-            '\u0020', '\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u0027',
+            new Rune(0x0020), new Rune(0x0021), new Rune(0x0022), new Rune(0x0023), new Rune(0x0024), new Rune(0x0025),
+            new Rune(0x0026), new Rune(0x0027),
 
             // 0x28
-            '\u0028', '\u0029', '\u002A', '\u002B', '\u002C', '\u002D', '\u002E', '\u002F',
+            new Rune(0x0028), new Rune(0x0029), new Rune(0x002A), new Rune(0x002B), new Rune(0x002C), new Rune(0x002D),
+            new Rune(0x002E), new Rune(0x002F),
 
             // 0x30
-            '\u0030', '\u0031', '\u0032', '\u0033', '\u0034', '\u0035', '\u0036', '\u0037',
+            new Rune(0x0030), new Rune(0x0031), new Rune(0x0032), new Rune(0x0033), new Rune(0x0034), new Rune(0x0035),
+            new Rune(0x0036), new Rune(0x0037),
 
             // 0x38
-            '\u0038', '\u0039', '\u003A', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F',
+            new Rune(0x0038), new Rune(0x0039), new Rune(0x003A), new Rune(0x003B), new Rune(0x003C), new Rune(0x003D),
+            new Rune(0x003E), new Rune(0x003F),
 
             // 0x40
-            '\u0040', '\u0041', '\u0042', '\u0043', '\u0044', '\u0045', '\u0046', '\u0047',
+            new Rune(0x0040), new Rune(0x0041), new Rune(0x0042), new Rune(0x0043), new Rune(0x0044), new Rune(0x0045),
+            new Rune(0x0046), new Rune(0x0047),
 
             // 0x48
-            '\u0048', '\u0049', '\u004A', '\u004B', '\u004C', '\u004D', '\u004E', '\u004F',
+            new Rune(0x0048), new Rune(0x0049), new Rune(0x004A), new Rune(0x004B), new Rune(0x004C), new Rune(0x004D),
+            new Rune(0x004E), new Rune(0x004F),
 
             // 0x50
-            '\u0050', '\u0051', '\u0052', '\u0053', '\u0054', '\u0055', '\u0056', '\u0057',
+            new Rune(0x0050), new Rune(0x0051), new Rune(0x0052), new Rune(0x0053), new Rune(0x0054), new Rune(0x0055),
+            new Rune(0x0056), new Rune(0x0057),
 
             // 0x58
-            '\u0058', '\u0059', '\u005A', '\u005B', '\u00A3', '\u005D', '\u2191', '\u2190',
+            new Rune(0x0058), new Rune(0x0059), new Rune(0x005A), new Rune(0x005B), new Rune(0x00A3), new Rune(0x005D),
+            new Rune(0x2191), new Rune(0x2190),
 
             // 0x60
-            '\u2500', '\u2660', '\u2502', '\u2500', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x1FB79), new Rune(0x2660), new Rune(0x1FB72), new Rune(0x1FB78), new Rune(0x1FB77),
+            new Rune(0x1FB76), new Rune(0x1FB7A), new Rune(0x1FB71),
 
             // 0x68
-            '\u0000', '\u256E', '\u2570', '\u256F', '\u0000', '\u2572', '\u2571', '\u0000',
+            new Rune(0x1FB74), new Rune(0x256E), new Rune(0x2570), new Rune(0x256F), new Rune(0x1FB7C),
+            new Rune(0x2572), new Rune(0x2571), new Rune(0x1FB7D),
 
             // 0x70
-            '\u0000', '\u2022', '\u0000', '\u2665', '\u0000', '\u256D', '\u2573', '\u25CB',
+            new Rune(0x1FB7E), new Rune(0x25CF), new Rune(0x1FB7B), new Rune(0x2665), new Rune(0x1FB70),
+            new Rune(0x256D), new Rune(0x2573), new Rune(0x25CB),
 
             // 0x78
-            '\u2663', '\u0000', '\u2666', '\u253C', '\u0000', '\u2502', '\u03C0', '\u25E5',
+            new Rune(0x2663), new Rune(0x1FB75), new Rune(0x2666), new Rune(0x253C), new Rune(0x1FB8C),
+            new Rune(0x2502), new Rune(0x03C0), new Rune(0x25E5),
 
             // 0x80
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x88
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u2028', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x2028),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x90
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0x98
-            '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000), new Rune(0x0000),
+            new Rune(0x0000), new Rune(0x0000),
 
             // 0xA0
-            '\u00A0', '\u258C', '\u2584', '\u2594', '\u2581', '\u258E', '\u2592', '\u0000',
+            new Rune(0x00A0), new Rune(0x258C), new Rune(0x2584), new Rune(0x2594), new Rune(0x2581), new Rune(0x258E),
+            new Rune(0x2592), new Rune(0x2595),
 
             // 0xA8
-            '\u0000', '\u25E4', '\u258A', '\u251C', '\u2597', '\u2514', '\u2510', '\u2582',
+            new Rune(0x1FB8F), new Rune(0x25E4), new Rune(0x1FB87), new Rune(0x251C), new Rune(0x2597),
+            new Rune(0x2514), new Rune(0x2510), new Rune(0x2582),
 
             // 0xB0
-            '\u250C', '\u2534', '\u252C', '\u2524', '\u258E', '\u258D', '\u0000', '\u0000',
+            new Rune(0x250C), new Rune(0x2534), new Rune(0x252C), new Rune(0x2524), new Rune(0x258E), new Rune(0x258D),
+            new Rune(0x1FB88), new Rune(0x1FB82),
 
             // 0xB8
-            '\u0000', '\u2583', '\u0000', '\u2596', '\u259D', '\u2518', '\u2598', '\u259A',
+            new Rune(0x1FB83), new Rune(0x2583), new Rune(0x1FB7F), new Rune(0x2596), new Rune(0x259D),
+            new Rune(0x2518), new Rune(0x2598), new Rune(0x259A),
 
             // 0xC0 (repeats 0x60 - 0x67)
-            '\u2500', '\u2660', '\u2502', '\u2500', '\u0000', '\u0000', '\u0000', '\u0000',
+            new Rune(0x1FB79), new Rune(0x2660), new Rune(0x1FB72), new Rune(0x1FB78), new Rune(0x1FB77),
+            new Rune(0x1FB76), new Rune(0x1FB7A), new Rune(0x1FB71),
 
             // 0xC8 (repeats 0x68 - 0x6F)
-            '\u0000', '\u256E', '\u2570', '\u256F', '\u0000', '\u2572', '\u2571', '\u0000',
+            new Rune(0x1FB74), new Rune(0x256E), new Rune(0x2570), new Rune(0x256F), new Rune(0x1FB7C),
+            new Rune(0x2572), new Rune(0x2571), new Rune(0x1FB7D),
 
             // 0xD0 (repeats 0x70 - 0x77)
-            '\u0000', '\u2022', '\u0000', '\u2665', '\u0000', '\u256D', '\u2573', '\u25CB',
+            new Rune(0x1FB7E), new Rune(0x25CF), new Rune(0x1FB7B), new Rune(0x2665), new Rune(0x1FB70),
+            new Rune(0x256D), new Rune(0x2573), new Rune(0x25CB),
 
             // 0xD8 (repeats 0x78 - 0x7F)
-            '\u2663', '\u0000', '\u2666', '\u253C', '\u0000', '\u2502', '\u03C0', '\u25E5',
+            new Rune(0x2663), new Rune(0x1FB75), new Rune(0x2666), new Rune(0x253C), new Rune(0x1FB8C),
+            new Rune(0x2502), new Rune(0x03C0), new Rune(0x25E5),
 
             // 0xE0 (repeats 0xA0 - 0xA7)
-            '\u00A0', '\u258C', '\u2584', '\u2594', '\u2581', '\u258E', '\u2592', '\u0000',
+            new Rune(0x00A0), new Rune(0x258C), new Rune(0x2584), new Rune(0x2594), new Rune(0x2581), new Rune(0x258E),
+            new Rune(0x2592), new Rune(0x2595),
 
             // 0xE8 (repeats 0xA8 - 0xAF)
-            '\u0000', '\u25E4', '\u258A', '\u251C', '\u2597', '\u2514', '\u2510', '\u2582',
+            new Rune(0x1FB8F), new Rune(0x25E4), new Rune(0x1FB87), new Rune(0x251C), new Rune(0x2597),
+            new Rune(0x2514), new Rune(0x2510), new Rune(0x2582),
 
             // 0xF0 (repeats 0xB0 - 0xB7)
-            '\u250C', '\u2534', '\u252C', '\u2524', '\u258E', '\u258D', '\u0000', '\u0000',
+            new Rune(0x250C), new Rune(0x2534), new Rune(0x252C), new Rune(0x2524), new Rune(0x258E), new Rune(0x258D),
+            new Rune(0x1FB88), new Rune(0x1FB82),
 
             // 0xF8 (repeats 0xB8 - 0xBF)
-            '\u0000', '\u2583', '\u0000', '\u2596', '\u259D', '\u2518', '\u2598', '\u259A'
+            new Rune(0x1FB83), new Rune(0x2583), new Rune(0x1FB7F), new Rune(0x2596), new Rune(0x259D),
+            new Rune(0x2518), new Rune(0x2598), new Rune(0x259A)
         };
 
         /// <summary>Converts a Unicode character to an PETSCII character</summary>
         /// <returns>PETSCII character.</returns>
         /// <param name="character">Unicode character.</param>
-        private protected override byte GetByte(char character)
+        private protected override byte GetByte(Rune character)
         {
-            if(character == 0x0000)
+            if(character.Value == 0x0000)
                 return 0x3F;
 
             for(int i = 0; i < 256; i++)
